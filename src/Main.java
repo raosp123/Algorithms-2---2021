ad2d25fdbaa505f9535d35994ec5a2da1a55758e
Sai.7 <kanthras@tcd.ie>
2021-04-27 15:36:42 +0100

    Implemented TST part of the interface

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
+
 		for(int i=0;i<args.length;i++) {
-			System.out.println(args[0]);
+			System.out.println(args[i]);
+		}
+
+		if (args[0].equalsIgnoreCase("TST"))
+		{
+			try
+			{
+				TST<Integer> tree = new TST<Integer>();
+				String input = args[1];
+				for (int i = 2; i < args.length; i++) {
+					input = input + " " + args[i];
+				}
+				tree.activateSearchSystem(input.toUpperCase());
+			}
+			catch(ArrayIndexOutOfBoundsException e)
+			{
+				System.out.println("Enter TST followed by a bus stop name to be searched");
+			}
+		}
+
+		else
+		{
+			System.out.println("Invalid Input. Enter SP or TST or SBAT");
 		}
 	}
 
