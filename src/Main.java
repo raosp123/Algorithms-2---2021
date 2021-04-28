1e6b57052adfb040e02f7c17daf507b2868b628c
Sai.7 <kanthras@tcd.ie>
2021-04-28 13:57:48 +0100

    Updated main

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,31 @@
 import java.io.IOException;
-import java.sql.Time;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		
-		searchByTime xx = new searchByTime();
-		
-		xx.mufat("5:25:00");
-		
-		
-		
-/*
-		for(int i=0;i<args.length;i++) {
-			System.out.println(args[i]);
+
+		if(args[0].equalsIgnoreCase("SP")) {
+			try
+			{
+				int sourceStop = Integer.parseInt(args[1]);
+				int targetStop = Integer.parseInt(args[2]);
+
+				//System.out.println("Source Stop is: "+ sourceStop + "\nTarget Stop is: "+ targetStop);
+
+				fileReader reader = new fileReader();
+				EdgeWeightedDigraph G = reader.returnGraph();
+
+				DijkstraSP sp = new DijkstraSP(G,sourceStop);
+				String path = sp.shortestPath(sourceStop, targetStop);
+
+				System.out.println(path);
+			}
+			catch(ArrayIndexOutOfBoundsException e) {
+				throw new IllegalArgumentException("Please enter valid stop IDs");
+			}
 		}
 
-		if (args[0].equalsIgnoreCase("TST"))
+		else if (args[0].equalsIgnoreCase("TST"))
 		{
 			try
 			{
@@ -29,15 +38,25 @@ public class Main {
 			}
 			catch(ArrayIndexOutOfBoundsException e)
 			{
-				System.out.println("Enter TST followed by a bus stop name to be searched");
+				throw new IllegalArgumentException("Please enter valid stop name");
 			}
 		}
 
-		else
+		else if(args[0].equalsIgnoreCase("SBAT"))
 		{
-			System.out.println("Invalid Input. Enter SP or TST or SBAT");
+			try
+			{
+				searchByTime search = new searchByTime();
+				search.getAllTrips(args[1]);
+			}
+			catch(ArrayIndexOutOfBoundsException e)
+			{
+				throw new IllegalArgumentException("Please enter valid time");
+			}
 		}
-	} */
+
+		else {
+			throw new IllegalArgumentException("Invalid Command-Line Inputs, Please Enter SP or TST or SBAT");
+		}
 	}
-	
 }
