eb22e8e933ef25b173a6af8d01214852ce8a3f3e
DoussotN <78543975+DoussotN@users.noreply.github.com>
2021-04-30 15:07:07 +0100

    changed comments

--- src/searchByTime.java
+++ src/searchByTime.java
@@ -10,24 +10,7 @@ import java.util.Collections;
 
 public class searchByTime {
 
-	//read from file into arraylist?, and copy all the trip IDS for the user inputted arrival time into a seperate list
-	
-	//error checking for invalid times happens at the same time
 
-	
-	//go back through the arraylist and push all the entries with matching trip IDS into some other data format
-	
-	//or just print them for now because I'm lazy
-	
-	
-	
-	
-	//method to get data in the format I want.
-	//should merge this with fileReader class later
-	
-	
-
-	
 	//input String should be in format dd:hh:mm like in the spec sheet
 	public void getAllTrips(String userInput) {	
 		
@@ -42,24 +25,20 @@ public class searchByTime {
 	        
 	        String currLine = reader.readLine();
 	        
-	        //N array accesses
+	        
 	        while (currLine!=null)
 	        {
 	        	
-	            String[] splitLine = currLine.trim().split("[,]");          
+	            String[] splitLine = currLine.trim().split("[,]");   
+	            
 	            // check if it matches the user inputed time
 	            //if it does then off the trip ID goes into another arrayList.
-	            if(splitLine[1].trim().equals(userInput.trim())) {
+	            if(splitLine[1].trim().equals(userInput.trim())) 
+	            {         	
 	            	tripIDlist.add(splitLine[0].trim());
 	            }
-	            	
-	            	
 	            
-	            
-	           	//I think i'm meant to do something here but I forget :)
-	            
-	            currLine=reader.readLine();
-	            
+	            currLine=reader.readLine();	  
 	        }
 	        
 	        //do it all again this time shoving all the relevant data into an array based on trip ID,
@@ -74,33 +53,26 @@ public class searchByTime {
         		if (Integer.parseInt(splitDate[0]) < 24  && Integer.parseInt(splitDate[1]) < 60 && Integer.parseInt(splitDate[2]) < 60)
         		{
 		        	for(int i = 0; i < tripIDlist.size(); i++)
-		        	{  
-		        		
+		        	{  	        		
 		        		if (splitLine[0].equals(tripIDlist.get(i)))
 		        		{
-		        			//System.out.println(currLine);
 		        			parsed_stop_times.add(currLine);
 		        		}
 		        	}
         		}
-	            currLine=reader.readLine();
-        		
+	            currLine=reader.readLine();        		
 	        }
+
 	        
-
-
 	        if (parsed_stop_times.size() != 0)
 	        {
-	        	
-	        	
-		        
-		        
-		        Collections.sort(parsed_stop_times);
+	        	Collections.sort(parsed_stop_times);
                 for(String s: parsed_stop_times)
                     System.out.println(s);
 	        }
 	        else
 	        	System.out.println("There are no stop times matching the input.");
+	        
 		}
         catch (FileNotFoundException e) {
             e.printStackTrace();
